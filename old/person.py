7937f73145acfe4394982b87faa3aae9b4cc73e0
Craig Chambers <craigchambers2101@gmail.com>
2022-05-30 14:18:51 +0100

    CRUD database example

--- old/person.py
+++ old/person.py
@@ -1,8 +1,12 @@
 class Person:
     def __init__(self, firstname, surname, age) -> None:
-        self.firstName = firstname
+        self.first_name = firstname
         self.surname = surname
         self.age = age
 
     def __str__(self) -> str:
-        return f'{self.firstname} {self.surname}'
+        return f'{self.first_name} {self.surname}'
+
+    def test():
+        pass
+
